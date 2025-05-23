bd274c587e84ea9ec853088927a4b4ec28f6a05b
MushiKS <srtimoomega@gmail.com>
2025-05-23 12:54:41 -0300

    Create 07-while.py

---
+++ tipos/contro-flujo/07-while.py
@@ -0,0 +1,16 @@
+# numero = 1
+# while numero <100:
+#     print(numero)
+#     numero *= 2 # también se puede hacer de numero = * 2
+    
+# comando = ""
+
+# while comando.lower() != "salir":
+#     comando = input("$ ")
+#     print(comando)
+
+
+comando = ""
+while True :
+    comando = input("$ ")
+    print(comando)
